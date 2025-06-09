879836ed431f964afa313975f15f92871761ab32
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-06-08 18:47:48 -0700

    Add buttons for easily deleting past chats

--- modules/chat.py
+++ modules/chat.py
@@ -1563,7 +1563,6 @@ def handle_delete_chat_confirm_click(state):
         unique_id,
         gr.update(visible=False),
         gr.update(visible=True),
-        gr.update(visible=False)
     ]
 
 
